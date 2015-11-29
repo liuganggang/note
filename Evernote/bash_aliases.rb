2e6e4befe02d2d2a7d80f0ca4a6cc86479ee1f1e
liugang <501225658@qq.com>
2015-11-29 20:26:54 +0800

    update aliases

--- Evernote/bash_aliases.rb
+++ Evernote/bash_aliases.rb
@@ -5,14 +5,24 @@ fi
 
 # in .bash_aliases file
 
-alias ttr='touch tmp/restart.txt'
+# git
+alias ggs='git status'
+alias gpr='git pull --rebase'
+
+# rails command
+alias ras='bundle exec rails s'
+alias rac='bundle exec rails c'
+alias ras1='bundle exec rails s -p 4000'
+alias ras2='bundle exec rails s -p 5000'
+alias ras3='bundle exec rails s -p 6000'
+alias ras4='bundle exec rails s -p 7000'
+
 alias rdm='RAILS_ENV=production bundle exec rake db:migrate'
 alias rap='RAILS_ENV=production bundle exec rake assets:precompile'
+alias ttr='touch tmp/restart.txt'
 alias tf="tail -f log/production.log"
-alias gpox='git pull origin sun_ego'
+
 alias sunstart="RAILS_ENV=production bundle exec rake sunspot:solr:start"
 alias sunstop="RAILS_ENV=production bundle exec rake sunspot:solr:stop"
 alias sunreindex='RAILS_ENV=production bundle exec rake sunspot:solr:reindex'
-alias railsc="rails c production"
 alias rcc="RAILS_ENV=production bundle exec rake cache:clear"
-alias sun_ego='cd /yigou/sun_ego'
