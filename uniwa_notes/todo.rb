eed6fae068e989fdf8138f73b4121dec7101a89d
liugang <501225658@qq.com>
2017-11-18 18:02:34 +0800

    todo

--- uniwa_notes/todo.rb
+++ uniwa_notes/todo.rb
@@ -1,2 +1,22 @@
 # 2017-11-13
 待测试： 讲师直播申请 权限控制
+
+# 2017-11-16
+Pub::Video.find_each {|x| x.copy_lecturer_poster!}
+
+# 小米IP
+119.57.108.112
+
+# 关注
+讲师 /follow_v21
+直播 POST /lecturer_feeds/follow_lecturer_v1
+
+# 我的收藏
+GET /users/collections
+# 我的留言
+GET /users/my_message_new
+
+
+
+
+
