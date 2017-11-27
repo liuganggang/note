c616f1dd8201ed38f6e1f7d7d1547bd57a2d3d73
liugang <501225658@qq.com>
2017-11-27 19:07:51 +0800

    todo

--- uniwa_notes/todo.rb
+++ uniwa_notes/todo.rb
@@ -23,5 +23,32 @@ GET /users/my_message_new
 
 
 
+var mescroll0 = new MeScroll("mescroll0", {
+        down: {
+          auto: true, //是否在初始化完毕之后自动执行下拉回调callback; 默认true
+          callback: downCallback0 //下拉刷新的回调
+        },
+        up: {
+          auto: false, //是否在初始化时以上拉加载的方式自动加载第一页数据; 默认false
+          callback: upCallback0, //上拉回调,此处可简写; 相当于 callback: function (page) { upCallback(page); }
+          onScroll: scrollm,
+          toTop:{ //配置回到顶部按钮
+            src: "<%= image_url 'custom/pub/v1/mescroll-totop.png' %>" //默认滚动到1000px显示,可配置offset修改
+          }
+        }
+      });
 
+      function scrollm(mescroll, y, isUp){
+        console.log(isUp)
+        console.log(y)
+        // 上滑
+        if(isUp == true && window.scrollY < 131 && y < 131){
+          window.scrollTo(0, y);
+        }
+
+        // 下滑
+        if(isUp == false && window.scrollY > 130 && y == 0 ){
+          window.scrollTo(0,0);
+        }
+      }
 
