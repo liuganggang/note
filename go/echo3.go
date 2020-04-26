7fc87f00ebb6453ebc98d1c69457e1eb034bf90e
liugang <501225658@qq.com>
2020-04-26 10:13:34 +0800

    update

--- go/echo3.go
+++ go/echo3.go
@@ -6,8 +6,36 @@ import (
   "strings"
 )
 
+
 func main() {
+
+  var bi string = "bi"
+  var names []string
+  fmt.Println(bi)
+  fmt.Println(len(names))
 
   fmt.Println(strings.Join(os.Args[1:], " "))
   // fmt.Println(os.Args[1:])
+
+  fmt.Printf("like you %g = %g ", like(1.0), 2.0)
+
+  var e error
+  var p Point
+  i,j := 1,0
+  i,j = j,i
+
+  f, err = os.Open(name)
+
+  if f != nil {
+    return err
+  }
+  f.close()
+
+  in, err := os.Open(name)
+  out, err := os.Create(name) // 第二次赋值。 必须其中有一个参数是已经初始过了的才能赋值
+
+}
+
+func like(i float64) float64 {
+  return i + 1
 }
--- go/hello.go
+++ go/hello.go
@@ -4,6 +4,75 @@ import "fmt"
 
 func main() {
   fmt.Printf("hello world\n")
+
+  // x := 1
+  // p := &x // 指针， 指向x变量，int类型的指针
+  // fmt.Println(*p) // 指针 内存地址的值
+  // *p = 2
+  // fmt.Println(x)
+
+  var a, b int
+
+  fmt.Println(&a == &a, &a == &b, &a == nil)
+
+  v := 1
+  incr(&v)
+  fmt.Println(incr(&v))
+
+  p := new(int)
+  fmt.Println(*p)
+  *p = 2
+  fmt.Println(*p)
+
+  fmt.Println(newInt())
+  fmt.Println(fib(23))
+  names := []string{"gold", "silver", "bronze"}
+  fmt.Println(names)
 }
+
+
+func incr(p *int) int {
+  *p++
+  return *p
+}
+
+func newInt() *int {
+  return new(int)
+}
+
+func fib(n int) int {
+    x, y := 0, 1
+    for i := 0; i < n; i++ {
+        x, y = y, x+y
+    }
+    return x
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
