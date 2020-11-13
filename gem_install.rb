ee8bbb1665605ddcea558279a75492d0e2816c1b
liugang <liugang.dev@aliyun.com>
2020-11-13 16:44:34 +0800

    1212

--- gem_install.rb
+++ gem_install.rb
@@ -4,6 +4,12 @@ brew install imagemagick@6
 PKG_CONFIG_PATH=/usr/local/Cellar/imagemagick@6/6.9.9-51/lib/pkgconfig/ gem install rmagick -v '2.16.0'
 
 
+rvm install 2.3.0 --with-openssl-dir=$(brew --prefix openssl@1.0)
+
+brew install rbenv/tap/openssl@1.0
+rvm reinstall 2.3.8 --with-openssl-dir='/usr/local/opt/openssl@1.0'
+gem update --system
+
 # 命令行调试 错误界面监控
 gem 'pry-rails', '~> 0.3.5'
 gem 'better_errors', '~> 2.1', '>= 2.1.1'
